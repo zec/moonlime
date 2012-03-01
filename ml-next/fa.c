763d2c33ff6461122b4634e24581e185ac594d4c
Zachary Catlin <z@zc.is>
2012-02-29 23:26:54 -0500

    C version: First structures for finite-automaton operations

---
+++ ml-next/fa.c
@@ -0,0 +1,7 @@
+/*
+ * fa.c: Stuff relating to finite automata.
+ *
+ * Copyright © 2012 Zachary Catlin. See LICENSE for terms.
+ */
+
+#include "fa.h"
---
+++ ml-next/fa.h
@@ -0,0 +1,52 @@
+/*
+ * fa.h: Stuff relating to finite automata.
+ *
+ * Copyright © 2012 Zachary Catlin. See LICENSE for terms.
+ */
+
+#ifndef ML_FA_H
+#define ML_FA_H
+
+#ifndef ML_UTILS_H
+#include "utils.h"
+#endif
+
+typdef struct state_str state_t;
+
+/* A transition between states */
+struct transition {
+    int is_nil;                  /* Is this a nil-transition (typically denoted
+                                  * by epsilon; a transition in an NFA that
+                                  * always happens and consumes no chars) */
+    unsigned int cond[CLASS_SZ]; /* The set of characters that trigger this
+                                  * transition */
+    state_t *dest;               /* The state this transition goes to */
+    struct transition *next;     /* Next transition in a list */
+    struct transition *next_fin; /* Next transition in the list of final
+                                  * transitions for this NFA fragment */
+};
+
+typedef struct transition trans_t;
+
+/* A state in a finite automaton */
+struct state_str {
+    int id;           /* Unique in the automaton in question */
+    trans_t *trans;   /* Start of the list of transitions from this state */
+    state_t *next;    /* Next in the list of states in the automaton */
+};
+
+/* A finite automaton (NFA or DFA) */
+typedef struct {
+    int n_states;    /* Number of states currently in the automaton */
+    state_t *first;  /* First in the list of states in the automaton */
+    state_t *last;   /* The current tail of the list of states */
+} fa_t;
+
+/* A fragment of an NFA corresponding to part of a regular expression */
+typedef struct {
+    fa_t *fa;       /* The NFA this fragment is a part of */
+    state_t *init;  /* The initial state of this fragment */
+    trans_t *final; /* The list of transitions out of this fragment */
+} fa_frag_t;
+
+#endif
