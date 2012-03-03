def84c304e7018b93654751cfb1e24c74b586910
Zachary Catlin <z@zc.is>
2012-03-02 21:52:39 -0500

    C version: Completed first whack at regex_to_nfa_frag

--- ml-next/fa.c
+++ ml-next/fa.c
@@ -71,7 +71,9 @@ static fa_frag_t * regex_to_nfa_frag(regex_t *rx, fa_t *fa)
     fa_frag_t *subfrag;
     state_t *init_st = mkstate(fa);
     trans_t *tr = NULL;
+    trans_t *p;
     int i;
+    regex_t dummy;
 
     frag->fa = fa;
     frag->init = init_st;
@@ -105,22 +107,122 @@ static fa_frag_t * regex_to_nfa_frag(regex_t *rx, fa_t *fa)
         break;
 
       case R_OPTION:
-        ;
+        for(i = 0; i < rx->data.list.n_enc; ++i) {
+            subfrag = regex_to_nfa_frag(rx->data.list.enc[i], fa);
+            tr = mktrans(init_st, subfrag->init);
+            tr->is_nil = 1;
+            /* Prepend subfrag's outward-transition list to frag's */
+            p = subfrag->final;
+            while(p != NULL && p->next_fin != NULL)
+                p = p->next_fin;
+            if(p != NULL) {
+                p->next_fin = frag->final;
+                frag->final = subfrag->final;
+            }
+            free(subfrag);
+        }
         break;
 
       case R_CONCAT:
+        tr = mktrans(init_st, NULL);
+        tr->is_nil = 1;
+        frag->final = tr;
+
+        for(i = 0; i < rx->data.list.n_enc; ++i) {
+            subfrag = regex_to_nfa_frag(rx->data.list.enc[i], fa);
+            /* Take frag's current outward-transition list and point them to
+             * subfrag's initial state */
+            for(p = frag->final; p != NULL; p = p->next_fin)
+                p->dest = subfrag->init;
+            /* Make subfrag's outward-transition list frag's new
+             * outward-transition list */
+            frag->final = subfrag->final;
+            free(subfrag);
+        }
         break;
 
       case R_MAYBE:
+        subfrag = regex_to_nfa_frag(rx->data.enc, fa);
+        tr = mktrans(init_st, subfrag->init);
+        tr->is_nil = 1;
+
+        tr = mktrans(init_st, NULL);
+        tr->is_nil = 1;
+        tr->next_fin = subfrag->final;
+        frag->final = tr;
+
+        free(subfrag);
         break;
 
       case R_STAR:
+        subfrag = regex_to_nfa_frag(rx->data.enc, fa);
+        tr = mktrans(init_st, subfrag->init);
+        tr->is_nil = 1;
+
+        for(p = subfrag->final; p != NULL; p = p->next_fin)
+            p->dest = init_st;
+
+        tr = mktrans(init_st, NULL);
+        tr->is_nil = 1;
+        frag->final = tr;
+
+        free(subfrag);
         break;
 
       case R_PLUS:
+        subfrag = regex_to_nfa_frag(rx->data.enc, fa);
+
+        /* "init_st" is a bit of a misnomer in this case... */
+        frag->init = subfrag->init;
+
+        for(p = subfrag->final; p != NULL; p = p->next_fin)
+            p->dest = init_st;
+
+        tr = mktrans(init_st, NULL);
+        tr->is_nil = 1;
+        frag->final = tr;
+
+        free(subfrag);
         break;
 
       case R_NUM:
+        i = 0;
+
+        tr = mktrans(init_st, NULL);
+        tr->is_nil = 1;
+        frag->final = tr;
+
+        if(rx->data.num.min != -1) {
+            while(i < rx->data.num.min) {
+                subfrag = regex_to_nfa_frag(rx->data.num.enc, fa);
+                for(p = frag->final; p != NULL; p = p->next_fin)
+                    p->dest = subfrag->init;
+                frag->final = subfrag->final;
+                free(subfrag);
+                ++i;
+            }
+        }
+
+        if(rx->data.num.max != -1) {
+            dummy.type = R_MAYBE;
+            dummy.enc = rx->data.num.enc;
+            while(i < rx->data.max) {
+                subfrag = regex_to_nfa_frag(&dummy, fa);
+                for(p = frag->final; p != NULL; p = p->next_fin)
+                    p->dest = subfrag->init;
+                frag->final = subfrag->final;
+                free(subfrag);
+                ++i;
+            }
+        } else {
+            dummy.type = R_STAR;
+            dummy.enc = rx->data.num.enc;
+            subfrag = regex_to_nfa_frag(&dummy, fa);
+            for(p = frag->final; p != NULL; p = p->next_fin)
+                p->dest = subfrag->init;
+            frag->final = subfrag->final;
+            free(subfrag);
+        }
         break;
 
       case R_ZERO:
